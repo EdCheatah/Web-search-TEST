3079fa974330e682a3bc0e21a01a9b6b86fb29d0
EdCheatah <edjeruiz@gmail.com>
2026-01-04 00:07:15 -0400

    First commint from January 4

--- src/main/java/DemosPlaywright.java
+++ src/main/java/DemosPlaywright.java
@@ -3,5 +3,8 @@ public class DemosPlaywright {
         //Role, Context and goal
         //Adding new commits
         //trying something
+        System.out.println("Hello");
+        int newVector[] = {1,2,3,4};
+        System.out.println(newVector[0]);
     }
 }
